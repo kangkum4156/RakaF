3b4404a0829e034806bdea196ee8d1c672ef389d
jwjang126 <jwjang126@gmail.com>
2025-05-01 10:22:16 +0900

    signup_password text

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:rokafirst/login/signin.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:rokafirst/login/test.dart';
 import 'firebase_options.dart';
 
 void main() async {
